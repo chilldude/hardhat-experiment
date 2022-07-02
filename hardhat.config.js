b4315db80301bdefdacef875cb1a4f6993bad6fb
chilldude <tonychenkt@gmail.com>
2022-07-01 17:27:44 -0700

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,32 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  defaultNetwork: "hardhat",
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  networks: {
+    hardhat: {},
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/9bdeb21d951f4fd293179ea608a21c08",
+      accounts: ["0x{}"]
+    }
+  }
+};
